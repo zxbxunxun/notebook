3169ba767cd1de993f33ff3febac28bf9fe71b72
zxb <zxbxunxun@icloud.com>
2019-12-25 15:18:30 +0800

    style

--- node1/11.vue-router/vuedemo1/src/App.vue
+++ node1/11.vue-router/vuedemo1/src/App.vue
@@ -12,4 +12,26 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+* {
+  color: rgba(0, 0, 0, 0.8);
+}
+body,
+div,
+p,
+ul {
+  margin: 0;
+  padding: 0;
+}
+a {
+  text-decoration: none;
+  /* border-bottom: 2px solid rgba(255, 255, 255, 0.2); */
+}
+a.active {
+  border-bottom: 2px solid rgba(255, 0, 0, 0.2);
+  color: rgba(255, 0, 0, 0.7);
+}
+a:hover {
+  border-bottom: 2px solid rgba(0, 0, 0, 0.2);
+}
+</style>
